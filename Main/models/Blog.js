9883a164a0eea207f3afc383abb66c43c33c2af3
Greg Harris <ghart.productions@gmail.com>
2021-04-02 19:07:25 -0700

    created the Blog model

---
+++ Main/models/Blog.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model { }
+
+Blog.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        entry: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'traveller'
+    }
+);
+
+module.exports = Blog;
