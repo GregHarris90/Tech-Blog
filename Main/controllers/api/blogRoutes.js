6994c388a5b84df3476e4e563c894583cffb39d2
Greg Harris <ghart.productions@gmail.com>
2021-04-02 19:31:36 -0700

    finished index.js for api routes

---
+++ Main/controllers/api/blogRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { User, Blog, Comment } = require('../../models');
+
+
+
+
+module.exports = router;
--- Main/controllers/api/index.js
+++ Main/controllers/api/index.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
+
+const userRoutes = require('./userRoutes');
+const blogRoutes = require('./blogRoutes');
+const commentRoutes = require('./commentRoutes');
+
+router.use('/users', userRoutes);
+router.use('/blogs', blogRoutes);
+router.use('/comments', commentRoutes);
+
 // add additional api routes
 
 module.exports = router;
