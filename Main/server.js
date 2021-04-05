e0cf59b7c9262cac1cc6c543820af20ce6efd3bc
Greg Harris <ghart.productions@gmail.com>
2021-04-04 20:28:09 -0700

    uncommented server.js files & start building main.handlebars

--- Main/server.js
+++ Main/server.js
@@ -1,38 +1,38 @@
 // DEPENDENCIES *UNCOMMENT*
 const path = require('path');
 const express = require('express');
-// const session = require('express-session');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 // Set up Sequelize *UNCOMMENT*
 const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers *UNCOMMENT*
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
 // Session structure for Authentication *UNCOMMENT*
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 // Turn on Session *UNCOMMENT*
-// app.use(session(sess));
+app.use(session(sess));
 
 // Inform Express.js on which template engine to use *UNCOMMENT*
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // Turn on Express.json
 app.use(express.json());
