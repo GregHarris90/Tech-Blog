98a1ff55a6f117269b587c38c41dd34b79551e8a
Greg Harris <ghart.productions@gmail.com>
2021-06-03 00:22:50 -0700

    updated comment js

---
+++ Main/public/js/comment.js
@@ -0,0 +1,22 @@
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+
+    // Collect values from the comment post form
+    const comment = document.querySelector('#comment-post').value.trim();
+
+    // Send a POST request to the API endpoint
+    const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({ id, comment }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (response.ok) {
+        // If successful, redirect the browser to the dashboard page
+        document.location.replace('/dashboard');
+    } else {
+        alert('Failed to create comment!');
+    }
+};
