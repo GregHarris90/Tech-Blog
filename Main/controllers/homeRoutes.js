afbd98d4b7e8cb1e1a50f2b37506a8a9d0e1b012
Greg Harris <ghart.productions@gmail.com>
2021-06-02 23:06:20 -0700

    updated homeRoutes

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Blog, Comment } = require('../models');
+const { User, Blog } = require('../models');
 const withAuth = require('../utils/auth');
 
 // GET ALL ROUTE FOR BLOG POSTS
