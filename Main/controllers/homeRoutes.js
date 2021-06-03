26f4cded19285843635e7a9d04344188bc3a867b
Greg Harris <ghart.productions@gmail.com>
2021-06-02 23:02:35 -0700

    updated homeRoutes

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User, Blog, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-// get all blog posts
+// GET ALL ROUTE FOR BLOG POSTS
 router.get('/', async (req, res) => {
     try {
         const blogData = await Blog.findAll({
@@ -25,13 +25,51 @@ router.get('/', async (req, res) => {
     }
 });
 
+// GET BLOG BY ID
+router.get('/blog/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
 
+        const post = blogData.get({ plain: true });
 
+        res.render('project', {
+            ...post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
+// USE 'withAuth' MIDDLEWARE TO PREVENT ACCESS TO ROUTE
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        // Find the logged in user based on the session ID
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Project }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 // LOGIN ROUTE FOR USER
-
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
         res.redirect('/profile');
@@ -40,9 +78,6 @@ router.get('/login', (req, res) => {
 
     res.render('login');
 });
-
-
-
 
 module.exports = router;
 
