0610d7840b535be1bbe4f51c7108c04fcbfde8e9
Greg Harris <ghart.productions@gmail.com>
2021-06-03 00:31:51 -0700

    added comment routes

--- Main/controllers/api/commentRoutes.js
+++ Main/controllers/api/commentRoutes.js
@@ -1,7 +1,41 @@
 const router = require('express').Router();
-const { User, Blog, Comment } = require('../../models');
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
+// CREATE NEW BLOG POST
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
 
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// DELETE BLOG POST
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!commentData) {
+            res.status(404).json({ message: 'No comment found with this id!' });
+            return;
+        }
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 module.exports = router;
