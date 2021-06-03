9809a0d44a8249e74a4c0a075cedc40729d8b595
Greg Harris <ghart.productions@gmail.com>
2021-06-02 23:16:25 -0700

    updated sign js with comments

--- Main/public/js/signup.js
+++ Main/public/js/signup.js
@@ -1,11 +1,14 @@
+// SIGNUP FORM HANDLER
 const signupFormHandler = async (event) => {
     event.preventDefault();
 
+    // Collect values from the login form
     const name = document.querySelector('#signup-name').value.trim();
     const email = document.querySelector('#signup-email').value.trim();
     const password = document.querySelector('#signup-password').value.trim();
 
     if (name && email && password) {
+        // Send a POST request to the API endpoint
         const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({ name, email, password }),
@@ -13,6 +16,7 @@ const signupFormHandler = async (event) => {
         });
 
         if (response.ok) {
+            // If successful, redirect the browser to the profile page
             document.location.replace('/profile');
         } else {
             alert(response.statusText);
@@ -20,6 +24,7 @@ const signupFormHandler = async (event) => {
     }
 };
 
+// Event listener for new post button
 document
     .querySelector('.signup-form')
     .addEventListener('submit', signupFormHandler);
