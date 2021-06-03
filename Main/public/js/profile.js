a22d6b2be103c0020ab698991d03baaa2326e834
Greg Harris <ghart.productions@gmail.com>
2021-06-02 23:11:47 -0700

    updated profile js with comments

--- Main/public/js/profile.js
+++ Main/public/js/profile.js
@@ -1,33 +1,40 @@
+// BLOG FORM POST HANDLER
 const newFormHandler = async (event) => {
     event.preventDefault();
 
+    // Collect values from the login form
     const title = document.querySelector('#blog-title').value.trim();
     const post = document.querySelector('#blog-post').value.trim();
 
-        const response = await fetch(`/api/posts`, {
-            method: 'POST',
-            body: JSON.stringify({ title, post }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+    // Send a POST request to the API endpoint
+    const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({ title, post }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
 
-        if (response.ok) {
-            document.location.replace('/profile');
-        } else {
-            alert('Failed to create blog post!');
-        }
-    };
+    if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/profile');
+    } else {
+        alert('Failed to create blog post!');
+    }
+};
 
+// DELETE BUTTON HANDLER
 const delButtonHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
         const id = event.target.getAttribute('data-id');
 
+        // Send a DELETE request to the API endpoint
         const response = await fetch(`/api/projects/${id}`, {
             method: 'DELETE',
         });
 
         if (response.ok) {
+            // If successful, redirect the browser to the profile page
             document.location.replace('/profile');
         } else {
             alert('Failed to delete blog post!');
@@ -35,10 +42,12 @@ const delButtonHandler = async (event) => {
     }
 };
 
+// Event listener for new post button
 document
     .querySelector('.new-post-form')
     .addEventListener('submit', newFormHandler);
 
+// Event listener for delete button
 document
     .querySelector('.delete-blog-post')
     .addEventListener('click', delButtonHandler);
